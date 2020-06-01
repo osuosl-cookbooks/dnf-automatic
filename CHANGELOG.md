ee4e42a90cfcfeb44ed949679e9b1d05216c6840
Manatee <github@osuosl.org>
2020-06-01 17:47:30 +0000

    Automatic minor-level version bump to v1.1.0 by Jenkins

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ dnf-automatic CHANGELOG
 This file is used to list changes made in each version of the
 dnf-automatic cookbook.
 
+1.1.0 (2020-06-01)
+------------------
+- Chef 15 fixes
+
 1.0.1 (2020-01-10)
 ------------------
 - Chef 14 post-migration fixes
--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,6 @@ issues_url       'https://github.com/osuosl-cookbooks/dnf-automatic/issues'
 license          'Apache-2.0'
 chef_version     '>= 14.0'
 description      'Installs/Configures dnf-automatic'
-version          '1.0.1'
+version          '1.1.0'
 
 supports         'centos', '~> 8.0'
