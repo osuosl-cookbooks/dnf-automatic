7511f779641c051bd218524ab00ba4b4b6e6fd47
Frank Nguyen <frankreed@osuosl.edu>
2022-03-03 19:28:13 -0800

    removed centos 8

--- metadata.rb
+++ metadata.rb
@@ -8,5 +8,4 @@ chef_version     '>= 16.0'
 description      'Installs/Configures dnf-automatic'
 version          '2.0.1'
 
-supports         'centos', '~> 8.0'
 supports         'centos_stream', '~> 8.0'
